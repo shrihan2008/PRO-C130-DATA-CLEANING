7cb6935f1f6f605fdf87b48c817097a50f424822
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-07-19 16:58:13 +0530

    Add files via upload

--- pro130.py
+++ pro130.py
@@ -1,21 +1,21 @@
 import csv
 data=[]
-with open('merged1.csv','r') as f:
+with open('merge1.csv','r') as f:
     read=csv.reader(f)
     for i in read:
         data.append(i)
 
-
+f=f[f['column_name'].notna()]
 
 del f["Luminiosity"]
 
+    
 file=f.rename({
     'Star_name':"Star_name",
     'Distance':"Distance",
     'Mass':'Mass',
     'Radius':"Radius",
-   
-}
+},
 axis='columns')
 
 print(file.shape)
