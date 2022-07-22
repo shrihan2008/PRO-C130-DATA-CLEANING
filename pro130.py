27359402c0e7dc63ae7b50278e915bce1ea3123e
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-07-22 22:28:51 +0530

    Add files via upload

--- pro130.py
+++ pro130.py
@@ -7,9 +7,11 @@ with open('merge1.csv','r') as f:
 nan = f
 f= f.dropna()
 f=f[f['column_name'].notna()]
+nan = data 
+data=f.dropna()
+columns = f.columns
 
-
-del f["Luminiosity"]
+f.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
 
     
 file=f.rename({
