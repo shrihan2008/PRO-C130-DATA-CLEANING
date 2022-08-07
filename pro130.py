b6e18913f2a7936972296211e52b9ae8f2528972
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-08-07 13:56:14 +0530

    Add files via upload

--- pro130.py
+++ pro130.py
@@ -12,6 +12,11 @@ columns = f.columns
 
 f.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
 
+final_data = f.dropna()
+
+final_data.reset_index(drop=True,inplace = True)
+
+final_data.to_csv('final_data.csv')
     
 file=f.rename({
     'Star_name':"Star_name",
