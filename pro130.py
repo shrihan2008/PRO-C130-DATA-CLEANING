62ca905c8bb98bf9f0e80813a488b25374f52d75
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-08-07 18:21:36 +0530

    Add files via upload

--- pro130.py
+++ pro130.py
@@ -1,13 +1,11 @@
-import csv
+import csv,pandas as pd
 data=[]
-with open('merge1.csv','r') as f:
-    read=csv.reader(f)
-    for i in read:
-        data.append(i)
-
-f=f[f['Radius'].notna()]
-f=f[f['Mass'].notna()]
-f=f[f['Distance'].notna()]
+f=pd.read_csv("dwarf_stars.csv")
+star_data=data[1:]
+star_data.sort(key=lambda star_data:star_data[2])
+f=f[f['Radius']]
+f=f[f['Mass']]
+f=f[f['Distance']]
 columns = f.columns
 
 f.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
