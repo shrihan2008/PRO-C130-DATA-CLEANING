d9809d385aac60a35f1a4f7e6e56f9e463b4d5cd
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-08-15 13:39:03 +0530

    Add files via upload

--- pro130.py
+++ pro130.py
@@ -1,21 +1,21 @@
 import csv,pandas as pd
+from turtle import distance
 data=[]
 f=pd.read_csv("total_stars.csv")
 
 star_data=data[1:]
 star_data.sort(key=lambda star_data:star_data[2])
-f=f[f['Radius']]
-f=f[f['Mass']]
-f=f[f['Distance']]
+
+mass=f['Mass'].to_list()
+radius =f['Radius'].to_list()
+d =f['Distance'].to_list()
+
+f.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
+
+final_data = f.dropna()
+final_data.reset_index(drop=True,inplace = True)
 
 
-#f.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
-
-#final_data = f.dropna()
-
-#f#inal_data.reset_index(drop=True,inplace = True)
-
-#final_data.to_csv('final_data.csv')
     
 file=f.rename({
     'Star_name':"Star_name",
@@ -26,4 +26,4 @@ file=f.rename({
 axis='columns')
 
 print(file.shape)
-file.to_csv("main.csv"),
+file.to_csv("e.csv"),
